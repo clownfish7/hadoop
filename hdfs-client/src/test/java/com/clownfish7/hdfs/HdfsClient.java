673d6e3afbdbfd7a33a5199a05cba593a597c560
clownfish7 <279505647@qq.com>
2021-01-23 17:46:11 +0800

    hdfs-client

--- hdfs-client/src/test/java/com/clownfish7/hdfs/HdfsClient.java
+++ hdfs-client/src/test/java/com/clownfish7/hdfs/HdfsClient.java
@@ -1,12 +1,16 @@
 package com.clownfish7.hdfs;
 
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.*;
+import org.apache.hadoop.io.IOUtils;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URI;
+import java.net.URISyntaxException;
 
 /**
  * classname HdfsClient
@@ -34,4 +38,176 @@ public class HdfsClient {
         // 3. 关闭资源
         fs.close();
     }
+
+    @Test
+    public void testCopyFromLocalFile() throws IOException, InterruptedException, URISyntaxException {
+
+        // 1 获取文件系统
+        Configuration configuration = new Configuration();
+        configuration.set("dfs.replication", "2");
+        FileSystem fs = FileSystem.get(new URI("hdfs://192.168.0.24:9001"), configuration, "root");
+
+        // 2 上传文件
+        fs.copyFromLocalFile(new Path("e:/banzhang.txt"), new Path("/banzhang.txt"));
+
+        // 3 关闭资源
+        fs.close();
+
+        System.out.println("over");
+    }
+
+    @Test
+    public void testCopyToLocalFile() throws IOException, InterruptedException, URISyntaxException {
+
+        // 1 获取文件系统
+        Configuration configuration = new Configuration();
+        FileSystem fs = FileSystem.get(new URI("hdfs://192.168.0.24:9001"), configuration, "root");
+
+        // 2 执行下载操作
+        // boolean delSrc 指是否将原文件删除
+        // Path src 指要下载的文件路径
+        // Path dst 指将文件下载到的路径
+        // boolean useRawLocalFileSystem 是否开启文件校验
+        fs.copyToLocalFile(false, new Path("/banzhang.txt"), new Path("e:/banhua.txt"), true);
+
+        // 3 关闭资源
+        fs.close();
+    }
+
+    @Test
+    public void testDelete() throws IOException, InterruptedException, URISyntaxException {
+
+        // 1 获取文件系统
+        Configuration configuration = new Configuration();
+        FileSystem fs = FileSystem.get(new URI("hdfs://192.168.0.24:9001"), configuration, "root");
+
+        // 2 执行删除
+        fs.delete(new Path("/0508/"), true);
+
+        // 3 关闭资源
+        fs.close();
+    }
+
+    @Test
+    public void testRename() throws IOException, InterruptedException, URISyntaxException {
+
+        // 1 获取文件系统
+        Configuration configuration = new Configuration();
+        FileSystem fs = FileSystem.get(new URI("hdfs://192.168.0.24:9001"), configuration, "root");
+
+        // 2 修改文件名称
+        fs.rename(new Path("/banzhang.txt"), new Path("/banhua.txt"));
+
+        // 3 关闭资源
+        fs.close();
+    }
+
+    @Test
+    public void testListFiles() throws IOException, InterruptedException, URISyntaxException {
+
+        // 1获取文件系统
+        Configuration configuration = new Configuration();
+        FileSystem fs = FileSystem.get(new URI("hdfs://192.168.0.24:9001"), configuration, "root");
+
+        // 2 获取文件详情
+        RemoteIterator<LocatedFileStatus> listFiles = fs.listFiles(new Path("/"), true);
+
+        while (listFiles.hasNext()) {
+            LocatedFileStatus status = listFiles.next();
+
+            // 输出详情
+            // 文件名称
+            System.out.println(status.getPath().getName());
+            // 长度
+            System.out.println(status.getLen());
+            // 权限
+            System.out.println(status.getPermission());
+            // 分组
+            System.out.println(status.getGroup());
+
+            // 获取存储的块信息
+            BlockLocation[] blockLocations = status.getBlockLocations();
+            for (BlockLocation blockLocation : blockLocations) {
+                String[] hosts = blockLocation.getHosts();
+                for (String host : hosts) {
+                    System.out.println(host);
+                }
+            }
+            System.out.println("---------------------");
+        }
+
+        // 3 关闭资源
+        fs.close();
+    }
+
+    @Test
+    public void testListStatus() throws IOException, InterruptedException, URISyntaxException {
+
+        // 1 获取文件配置信息
+        Configuration configuration = new Configuration();
+        FileSystem fs = FileSystem.get(new URI("hdfs://192.168.0.24:9001"), configuration, "root");
+
+        // 2 判断是文件还是文件夹
+        FileStatus[] listStatus = fs.listStatus(new Path("/"));
+
+        for (FileStatus fileStatus : listStatus) {
+
+            // 如果是文件
+            if (fileStatus.isFile()) {
+                System.out.println("f:" + fileStatus.getPath().getName());
+            } else {
+                System.out.println("d:" + fileStatus.getPath().getName());
+            }
+        }
+
+        // 3 关闭资源
+        fs.close();
+    }
+
+    @Test
+    public void putFileToHDFS() throws IOException, InterruptedException, URISyntaxException {
+
+        // 1 获取文件系统
+        Configuration configuration = new Configuration();
+        FileSystem fs = FileSystem.get(new URI("hdfs://192.168.0.24:9001"), configuration, "root");
+
+        // 2 创建输入流
+        FileInputStream fis = new FileInputStream(new File("e:/banhua.txt"));
+
+        // 3 获取输出流
+        FSDataOutputStream fos = fs.create(new Path("/banhua.txt"));
+
+        // 4 流对拷
+        IOUtils.copyBytes(fis, fos, configuration);
+
+        // 5 关闭资源
+        IOUtils.closeStream(fos);
+        IOUtils.closeStream(fis);
+        fs.close();
+    }
+
+    // 文件下载
+    @Test
+    public void getFileFromHDFS() throws IOException, InterruptedException, URISyntaxException {
+
+        // 1 获取文件系统
+        Configuration configuration = new Configuration();
+        FileSystem fs = FileSystem.get(new URI("hdfs://192.168.0.24:9001"), configuration, "root");
+
+        // 2 获取输入流
+        FSDataInputStream fis = fs.open(new Path("/banhua.txt"));
+
+        // 3 获取输出流
+        FileOutputStream fos = new FileOutputStream(new File("e:/banhua.txt"));
+
+        // 4 流的对拷
+        IOUtils.copyBytes(fis, fos, configuration);
+
+        // 5 关闭资源
+        IOUtils.closeStream(fos);
+        IOUtils.closeStream(fis);
+        fs.close();
+    }
+
+
 }
